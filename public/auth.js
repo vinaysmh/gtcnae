be265029a0aef1f8909ecb276cf832cac7fae1e0
vinaysmh <vinaysmh@users.noreply.github.com>
2020-02-26 17:39:22 +0530

    Test

---
+++ public/auth.js
@@ -0,0 +1,36 @@
+function login() {
+    var ux = document.getElementById('email').value;
+    var uy = document.getElementById('pass').value;
+}
+
+function forg() {
+    alert("Are You sure to submit password reset request");
+    var z = document.getElementById('c-email').value;
+}
+
+function reg() {
+    alert("Are You sure to submit your details for registration");
+    var ra = document.getElementById('uname').value;
+    var rb = document.getElementById('rollno').value;
+    var rc = document.getElementById('fname').value;
+    var rd = document.getElementById('mobno').value;
+    var re = document.getElementById('fmobno').value;
+    var rf = document.getElementById('sem').value;
+    var rg = document.getElementById('semail').value;
+    var rh = document.getElementById('oopsy').value;
+}
+
+function addevent() {
+    var x = document.getElementById('email').value;
+    var y = document.getElementById('pass').value;
+}
+
+function alogin() {
+    var ax = document.getElementById('aemail').value;
+    var ay = document.getElementById('apass').value;
+}
+
+function clogin() {
+    var cx = document.getElementById('cemail').value;
+    var cy = document.getElementById('cpass').value;
+}
